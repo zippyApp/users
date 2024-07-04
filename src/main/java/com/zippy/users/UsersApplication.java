8b005196c88a6845429cdc1dbb94bf54d10cf6e9
jorge <locodeira1b3@gmail.com>
2024-07-04 08:15:44 -0500

    allow cross origin

--- src/main/java/com/zippy/users/UsersApplication.java
+++ src/main/java/com/zippy/users/UsersApplication.java
@@ -3,9 +3,11 @@ package com.zippy.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin(origins = "*")
 public class UsersApplication {
 
 	public static void main(String[] args) {
